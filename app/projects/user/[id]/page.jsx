4fb6e0b5d8b15bd17122628e394c070df306bffc
Yonatan Ariel <yonatanriel@gmail.com>
2023-11-19 14:42:27 +0200

    updating from next.js 13 to 14

--- app/projects/user/[id]/page.jsx
+++ app/projects/user/[id]/page.jsx
@@ -2,26 +2,36 @@
 import { useEffect, useState } from "react";
 import { getUserProjects } from "../../../../functions/frontendFunctions/apiCalls";
 import ProjectCard from "../../../../components/ui/projectCard";
+import Popup from "../../../../components/ui/popup";
 
 export default function Page({ params }) {
   const [userProjects, setUserProjects] = useState("");
   useEffect(() => {
     getUserProjects(params.id).then((res) => setUserProjects(res));
   }, []);
+
   return (
-    <div className=" flex gap-4 lg:justify-start justify-evenly flex-wrap py-20 p-1 lg:p-20">
-      {userProjects &&
-        userProjects?.map((p) => (
-          <ProjectCard
-            key={p.id}
-            projectName={p.name}
-            developerName={p.userId.name}
-            developerImg={p.userId.photo}
-            developerTitle={p.userId.occupation}
-            img={p.photo}
-            {...p}
-          />
-        ))}
-    </div>
+    <>
+      {userProjects?.length === 0 && (
+        <Popup
+          text={"Sorry, This user has no projects yet"}
+          goBackButton={true}
+        />
+      )}
+      <div className=" flex gap-4 lg:justify-start justify-evenly flex-wrap py-20 p-1 lg:p-20">
+        {userProjects &&
+          userProjects?.map((p) => (
+            <ProjectCard
+              key={p.id}
+              projectName={p.name}
+              developerName={p.userId.name}
+              developerImg={p.userId.photo}
+              developerTitle={p.userId.occupation}
+              img={p.photo}
+              {...p}
+            />
+          ))}
+      </div>
+    </>
   );
 }
