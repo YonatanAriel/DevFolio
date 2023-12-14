8d209d1ba91ed795bc68104ea7420694f38e28ad
Yonatan Ariel <yonatanriel@gmail.com>
2023-12-14 12:17:44 +0200

    deploy test

--- app/api/users/route.js
+++ app/api/users/route.js
@@ -5,11 +5,13 @@ import { User } from "../../../DL/models/User.model";
 export async function GET(req) {
   await connectToDB();
   const allUsers = await User.find();
-  return new NextResponse(JSON.stringify(allUsers), {
-    status: 200,
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
+  // return new NextResponse(JSON.stringify(allUsers), {
+  //   status: 200,
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //   },
+  // });
+  return NextResponse.json(allProjects, { status: 200 });
+
   // return new NextResponse(JSON.stringify(allUsers), { status: 200 });
 }
